2bc991b904a14e6c5bf607d650bfa0fd8818397c
liyyao <2813789717@qq.com>
2023-05-26 10:51:58 +0800

    Initial commit

---
+++ src/test/java/com/liyyao/utils/common/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyyao.utils.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
